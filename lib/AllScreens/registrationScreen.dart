a570f5c019e340621ce4a4ddc6b029097f8b84e0
Faisal Hatila <hatilas1994@gmail.com>
2021-09-06 19:38:35 +0500

    UI: done for register screen

---
+++ lib/AllScreens/registrationScreen.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:uberapp/AllScreens/mainScreen.dart';
+
+class RegistrationScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 65.0,
+                ),
+                Image(
+                  image: AssetImage("images/logo.png"),
+                  width: 390.0,
+                  height: 250.0,
+                  alignment: Alignment.center,
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                Text(
+                  "Regsiter as Rider",
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontFamily: "Brand Bold",
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 1.0,
+                      ),
+                      TextField(
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                            labelText: "Name",
+                            labelStyle: TextStyle(fontSize: 14.0),
+                            hintStyle: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 10.0,
+                            )),
+                        style: TextStyle(fontSize: 14.0),
+                      ),
+                      SizedBox(
+                        height: 1.0,
+                      ),
+                      TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                            labelText: "Email",
+                            labelStyle: TextStyle(fontSize: 14.0),
+                            hintStyle: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 10.0,
+                            )),
+                        style: TextStyle(fontSize: 14.0),
+                      ),
+                      SizedBox(
+                        height: 1.0,
+                      ),
+                      TextField(
+                        keyboardType: TextInputType.phone,
+                        decoration: InputDecoration(
+                            labelText: "Phone",
+                            labelStyle: TextStyle(fontSize: 14.0),
+                            hintStyle: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 10.0,
+                            )),
+                        style: TextStyle(fontSize: 14.0),
+                      ),
+                      SizedBox(
+                        height: 1.0,
+                      ),
+                      TextField(
+                        obscureText: true,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                            labelText: "Password",
+                            labelStyle: TextStyle(fontSize: 14.0),
+                            hintStyle: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 10.0,
+                            )),
+                        style: TextStyle(fontSize: 14.0),
+                      ),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.yellow),
+                        ),
+                        onPressed: () {
+                          print("Login Clicked");
+                        },
+                        child: Container(
+                          height: 50.0,
+                          child: Center(
+                            child: Text(
+                              "Create Account",
+                              style: TextStyle(
+                                  fontSize: 18.0,
+                                  fontFamily: "Brand Bold",
+                                  color: Colors.black),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    print("Button pressed");
+                  },
+                  child: Text(
+                    "Already have an Account? Login Here.",
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
