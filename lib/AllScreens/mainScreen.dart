942b467b3f63b3ff0e2374e8c82a9e5f4a7a66ed
Faisal Hatila <hatilas1994@gmail.com>
2021-09-06 16:44:20 +0500

    New File: mainScreen created and integrated

---
+++ lib/AllScreens/mainScreen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            "Main Screen",
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
